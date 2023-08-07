d341f044d5400d888435554262eafa1b494329c2
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-08-07 21:17:40 +0600

    starting commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const { default: axios } = require("axios");
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://holdInfo:WFItEn3apHj5j29q@cluster0.beeiwwt.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", async (req, res) => {
+  res.send("HoldInfo is Running");
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // create db
+    const holdInfoDB = client.db("holdInfoDB");
+
+    // create collection
+    const cryptoCollection = holdInfoDB.collection("crypto");
+
+    app.get("/fetch-and-store", async (req, res) => {
+      const response = await axios.get(`https://api.wazirx.com/api/v2/tickers`);
+      const data = response.data;
+      const dataArray = Object.values(data);
+      // Extract the top 10 tickers from the dataArray
+      const top10Tickers = dataArray.slice(0, 10);
+
+      await cryptoCollection.deleteMany({});
+      await cryptoCollection.insertMany(top10Tickers);
+      res.send(top10Tickers);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
